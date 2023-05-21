f543f0656149f73a921c32cec8d6d6688a728764
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-05-21 18:31:30 +0500

    Ques till 4 done

--- js/date-method.js
+++ js/date-method.js
@@ -1,305 +1,278 @@
-function ques1(){
-    // Write a program that displays current date and time in
-    // your browser.
-        
-    let curDate = new Date();
+function ques1() {
+  // Write a program that displays current date and time in
+  // your browser.
 
-    document.writeln("<h1>Current Date:</h1>");
-    document.writeln(`<p>${curDate}</p>`);
+  let curDate = new Date();
 
+  document.writeln("<h1>Current Date:</h1>");
+  document.writeln(`<p>${curDate}</p>`);
 }
 
-function ques2(){
-    // Write a program that alerts the current month in words.
-    // For example December.
-    let curDate = new Date();
-    let curMonth = curDate.getMonth();
+function ques2() {
+  // Write a program that alerts the current month in words.
+  // For example December.
+  let curDate = new Date();
+  let curMonth = curDate.getMonth();
 
-    let month = months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let month = (months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]);
 
-    console.log(`Current Month: ${month[curMonth]}`);
-    alert(`Current Month: ${month[curMonth]}`);
-
-    // document.writeln("<h1>Current Date:</h1>");
-    // document.writeln(`<p>${curDate}</p>`);
+  console.log(`Current Month: ${month[curMonth]}`);
+  alert(`Current Month: ${month[curMonth]}`);
 }
 
-function ques3(){
-//     Write a program that alerts the first 3 letters of the current
-// day, for example if today is Sunday then alert will show
-// Sun.
+function ques3() {
+  //     Write a program that alerts the first 3 letters of the current
+  // day, for example if today is Sunday then alert will show
+  // Sun.
 
-    var color = prompt("Enter the Color: ");
+  let curDate = new Date();
+  let curDay = curDate.getDay();
 
-    if(color === "red" || color === "Red"){
-        document.write("Signal color: <b>" + color + "</b><br><br><b>Message: </b>Must Stop");
-    }
-    else if(color === "yellow" || color === "Yellow") {
-        document.write("Signal color: <b>" + color + "</b><br><br><b>Message: </b>Read to move");
-    }
-    else if(color === "green" || color === "Green"){
-        document.write("Signal color: <b>" + color + "</b><br><br><b>Message: </b>Move now");
-    }
-    else{
-        document.write("You didn't answer.");
-    }
-
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  console.log(`Today is ${days[curDay]}`);
+  alert(`Today is ${days[curDay]}`);
 }
-function ques4(){
-//     Write a program that displays a message “It’s Fun day” if
-// its Saturday or Sunday today.
+function ques4() {
+  //     Write a program that displays a message “It’s Fun day” if
+  // its Saturday or Sunday today.
+  let curDate = new Date();
+  let curDay = curDate.getDay();
 
-    var fuel = +prompt("Input the remaining Fuel in your Car in liter: ");
-    // document.write(fuel);
-    console.log(fuel);
-    if(fuel < 0.25){
-        alert("Alert: \nPlease refill the fuel in your car.");
-    }
-    else if(fuel >= 0.25) {
-        alert("Don't take tension.");
-    }
-    else{
-       alert("You didn't answer.");
-    }
+  console.log(`curDay  ==== ${curDay} `);
 
+  if (curDay === 0 || curDay === 1) {
+    console.log(`It’s Fun day`);
+    alert(`It’s Fun day`);
+  }
 }
-function ques5(){
-//     Write a program that shows the message “First fifteen
-// days of the month” if the date is less than 16th of the month
-// else shows “Last days of the month”.
-    third_subject = +prompt("Enter Your Third Subject Marks: ");
+function ques5() {
+  //     Write a program that shows the message “First fifteen
+  // days of the month” if the date is less than 16th of the month
+  // else shows “Last days of the month”.
+  third_subject = +prompt("Enter Your Third Subject Marks: ");
 
-    // Input total Marks
-    total_marks =  +prompt("Enter Your total Marks: ");
+  // Input total Marks
+  total_marks = +prompt("Enter Your total Marks: ");
 
-    // Obtained Marks
-    obtained_marks = first_subject + second_subject + third_subject;
+  // Obtained Marks
+  obtained_marks = first_subject + second_subject + third_subject;
 
-    // Total Marks
-    percentage = ((obtained_marks / total_marks) * 100);
+  // Total Marks
+  percentage = (obtained_marks / total_marks) * 100;
 
+  if (percentage >= 80) {
+    grade = "A-one";
+    remarks = "Excellent";
+  } else if (percentage >= 70) {
+    grade = "A";
+    remarks = "Good";
+  } else if (percentage >= 60) {
+    grade = "B";
+    remarks = "You need to improve";
+  } else if (percentage < 60) {
+    grade = "Fail";
+    remarks = "Sorry";
+  } else {
+    alert("You didn't answer.");
+  }
 
-    if (percentage >= 80){
-        grade = "A-one";
-        remarks = "Excellent";
-    }
-    else if (percentage >= 70){
-        grade = "A";
-        remarks = "Good";
-    }
-    else if (percentage >= 60){
-        grade = "B";
-        remarks = "You need to improve";
-    }
-    else if (percentage < 60){
-        grade = "Fail";
-        remarks = "Sorry";
-    }
-    else {
-        alert("You didn't answer.")
-    }
-    
-    document.write("<h1>Marks Sheet</h1><br><br>");
-    document.write("<h4>Total marks: " + total_marks + "</h4>");
-    document.write("<h4>Marks obtained: " + obtained_marks + "</h4>");
-    document.write("<h4>Percentage: " + percentage + "%</h4>");
-    document.write("<h4>Grade: " + grade + "</h4>");
-    document.write("<h4>Remarks: " + remarks + "</h4>");
+  document.write("<h1>Marks Sheet</h1><br><br>");
+  document.write("<h4>Total marks: " + total_marks + "</h4>");
+  document.write("<h4>Marks obtained: " + obtained_marks + "</h4>");
+  document.write("<h4>Percentage: " + percentage + "%</h4>");
+  document.write("<h4>Grade: " + grade + "</h4>");
+  document.write("<h4>Remarks: " + remarks + "</h4>");
 }
-function ques7(){
-        
-    // Write a program that determines the minutes since
-    // midnight, Jan. 1, 1970 and assigns it to a variable that
-    // hasn't been declared beforehand. Use any variable you like
-    // to represent the Date object.
+function ques7() {
+  // Write a program that determines the minutes since
+  // midnight, Jan. 1, 1970 and assigns it to a variable that
+  // hasn't been declared beforehand. Use any variable you like
+  // to represent the Date object.
 }
-function ques7(){
-        
-        //  Guess game:
-        // Store a secret number (ranging from 1 to 10) in a variable.
-        // Prompt user to guess the secret number.
-        // a. If user guesses the same number, show “Bingo! Correct
-        // answer”.
-        // b. If the guessed number +1 is the secret number, show
-        // “Close enough to the correct answer”.
+function ques7() {
+  //  Guess game:
+  // Store a secret number (ranging from 1 to 10) in a variable.
+  // Prompt user to guess the secret number.
+  // a. If user guesses the same number, show “Bingo! Correct
+  // answer”.
+  // b. If the guessed number +1 is the secret number, show
+  // “Close enough to the correct answer”.
 
-    
-    var secret_no; 
-    
-    var guess_no, close_no;
-    
-    // First Store a secret no
-    secret_no = 5;
-   
-    // Then Inpu a guess no from user 
-    guess_no = +prompt("Input a Number, Which I Think in My Mind: \nRange( 1 - 10): ");
-    
-    close_no = guess_no + 1;
-    // console.log(guess_no);
-    // console.log(close_no);
+  var secret_no;
 
-    if (secret_no == guess_no){
-        alert("Bingo! Correct answer.");
-    }
-    else if (secret_no == close_no){
-        alert("Close enough to the correct answer.");
-    }
-    else if (secret_no < (guess_no + 2 )){
-        alert("Your number is too large.");
-    }
-    else if (secret_no > (guess_no - 2 )){
-        alert("Your number is too small.");
-    }
-    else{
-        alert("You didn't answer.");
-    }
+  var guess_no, close_no;
 
+  // First Store a secret no
+  secret_no = 5;
+
+  // Then Inpu a guess no from user
+  guess_no = +prompt(
+    "Input a Number, Which I Think in My Mind: \nRange( 1 - 10): "
+  );
+
+  close_no = guess_no + 1;
+  // console.log(guess_no);
+  // console.log(close_no);
+
+  if (secret_no == guess_no) {
+    alert("Bingo! Correct answer.");
+  } else if (secret_no == close_no) {
+    alert("Close enough to the correct answer.");
+  } else if (secret_no < guess_no + 2) {
+    alert("Your number is too large.");
+  } else if (secret_no > guess_no - 2) {
+    alert("Your number is too small.");
+  } else {
+    alert("You didn't answer.");
+  }
 }
 
-function ques8(){
-    //     Write a program to check whether the given number is
-    // divisible by 3. Show the message to the user if the number
-    // is divisible by 3.
+function ques8() {
+  //     Write a program to check whether the given number is
+  // divisible by 3. Show the message to the user if the number
+  // is divisible by 3.
 
+  var input_num, result;
 
-    var input_num, result; 
+  // Then Inpu a guess no from user
+  input_num = +prompt(
+    "Input a Number, I will confirm \n Your number is divisable by 3:"
+  );
 
-    // Then Inpu a guess no from user 
-    input_num = +prompt("Input a Number, I will confirm \n \Your number is divisable by 3:");
-    
-    result = (input_num  % 3);
+  result = input_num % 3;
 
-    if (result == 0){
-        document.write("Your given number " + input_num + " is divisable by 3.");
-    }
-    else if (result !== 0){
-        document.write("Your given number " + input_num + " is not divisable by 3.");
-    }
-    else{
-        alert("You didn't answer.");
-    }
+  if (result == 0) {
+    document.write("Your given number " + input_num + " is divisable by 3.");
+  } else if (result !== 0) {
+    document.write(
+      "Your given number " + input_num + " is not divisable by 3."
+    );
+  } else {
+    alert("You didn't answer.");
+  }
 }
 
-function ques9(){
+function ques9() {
+  //     Write a program that checks whether the given input is an
+  // even number or an odd number.
 
-    //     Write a program that checks whether the given input is an
-    // even number or an odd number.
+  var input_num, result;
 
-    var input_num, result; 
+  // Then Inpu a guess no from user
+  input_num = +prompt("Input a Number, to Check Even or Odd: ");
 
-    // Then Inpu a guess no from user 
-    input_num = +prompt("Input a Number, to Check Even or Odd: ");
-    
-    result = (input_num  % 2);
+  result = input_num % 2;
 
-    if (result == 0){
-        document.write("Your given number " + input_num + " is Even.");
-    }
-    else if (result !== 0){
-        document.write("Your given number " + input_num + " is Odd.");
-    }
-    else{
-        alert("You didn't answer.");
-    }
+  if (result == 0) {
+    document.write("Your given number " + input_num + " is Even.");
+  } else if (result !== 0) {
+    document.write("Your given number " + input_num + " is Odd.");
+  } else {
+    alert("You didn't answer.");
+  }
 }
 
-function ques10(){
+function ques10() {
+  //Write a program that takes temperature as input and
+  // shows a message based on following criteria
+  // a. T > 40 then “It is too hot outside.”
+  // b. T > 30 then “The Weather today is Normal.”
+  // c. T > 20 then “Today’s Weather is cool.”
+  // d. T > 10 then “OMG! Today’s weather is so Cool.”
 
-    //Write a program that takes temperature as input and
-    // shows a message based on following criteria
-    // a. T > 40 then “It is too hot outside.”
-    // b. T > 30 then “The Weather today is Normal.”
-    // c. T > 20 then “Today’s Weather is cool.”
-    // d. T > 10 then “OMG! Today’s weather is so Cool.”
+  var input_temp;
 
-    var input_temp;
-    
-    // Input temperature
-    input_temp = +prompt("Enter Temperature: ");
+  // Input temperature
+  input_temp = +prompt("Enter Temperature: ");
 
-    if (input_temp > 40){
-        alert("It is too hot outside.");
-    }
-    else if (input_temp > 30){
-        alert("The Weather today is Normal.");
-    }
-    else if (input_temp > 20){
-        alert("Today’s Weather is cool.");
-    }
-    else if (input_temp > 10){
-        alert("OMG! Today’s weather is so Cool");
-    }
-    else if(input_temp < 10 && input_temp > 0 ){
-        alert("Invalid State!")
-    }
-    else {
-        alert("You didn't answer.")
-    }
+  if (input_temp > 40) {
+    alert("It is too hot outside.");
+  } else if (input_temp > 30) {
+    alert("The Weather today is Normal.");
+  } else if (input_temp > 20) {
+    alert("Today’s Weather is cool.");
+  } else if (input_temp > 10) {
+    alert("OMG! Today’s weather is so Cool");
+  } else if (input_temp < 10 && input_temp > 0) {
+    alert("Invalid State!");
+  } else {
+    alert("You didn't answer.");
+  }
 }
 
-function ques11(){
-    //     Write a program to create a calculator for +,-,*, / & %
-    // using if statements. Take the following input:
-    // a. First number
-    // b. Second number
-    // c. Operation (+, -, *, /, %)
-    // Compute & show the calculated result to user.
-    var first_number, second_number, operator, result;
+function ques11() {
+  //     Write a program to create a calculator for +,-,*, / & %
+  // using if statements. Take the following input:
+  // a. First number
+  // b. Second number
+  // c. Operation (+, -, *, /, %)
+  // Compute & show the calculated result to user.
+  var first_number, second_number, operator, result;
 
-    alert(`Let's Start Calculation`);
-    // Input first_number
-    first_number = +prompt(`Enter Your First Number: `);
+  alert(`Let's Start Calculation`);
+  // Input first_number
+  first_number = +prompt(`Enter Your First Number: `);
 
-    // Input second_number
-    second_number = +prompt(`Enter Your Second Number: `);
-    
-    // Input operator
-    operator= prompt(`Enter Your operator(+, -, *, /, %): `);
+  // Input second_number
+  second_number = +prompt(`Enter Your Second Number: `);
 
-    if( operator === `*`){
-        // Multiplication
-        document.write(`<h1>Calculator:</h1><br><br>`);
-        result = first_number * second_number;
-        document.write(`<h3>Multiplication:</h3>`);
-        document.write(`${first_number} * ${second_number} = ` + result + `<br><br>`);
-        
-    }
-    else if( operator === `/`){
-        // Division
-        document.write(`<h1>Calculator:</h1><br><br>`);
-        result = first_number / second_number;
-        document.write(`<h3>Division:</h3>`);
-        document.write(`${first_number} / ${second_number} = ` + result + `<br><br>`);
-        
-    }
-    else if( operator === `%`){
-        // Modulus / Remainder
-        document.write(`<h1>Calculator:</h1><br><br>`);
-        result = first_number % second_number;
-        document.write(`<h3>Modulus / Remainder</h3>`); 
-        document.write(`${first_number} % ${second_number} = ` + result + `<br><br>`); 
-    }
-    else if( operator === `+`){
-        // Addition
-        document.write(`<h1>Calculator:</h1><br><br>`);
-        result = first_number + second_number;
-        document.write(`<h3>Addition:</h3>`);
-        document.write(`${first_number} + ${second_number} = ` + result + `<br><br>`);
-    }
-    else if( operator === `-`){
-        // Subtration
-        document.write(`<h1>Calculator:</h1><br><br>`);
-        result = first_number - second_number;
-        document.write(`<h3>Subtration:</h3>`);
-        document.write(`${first_number} - ${second_number} = ` + result + `<br><br>`);
-    }
-    else{
-        alert(`Sorry! you missing some Input.`)
-    }
-    
-   
-    
+  // Input operator
+  operator = prompt(`Enter Your operator(+, -, *, /, %): `);
 
-
-
+  if (operator === `*`) {
+    // Multiplication
+    document.write(`<h1>Calculator:</h1><br><br>`);
+    result = first_number * second_number;
+    document.write(`<h3>Multiplication:</h3>`);
+    document.write(
+      `${first_number} * ${second_number} = ` + result + `<br><br>`
+    );
+  } else if (operator === `/`) {
+    // Division
+    document.write(`<h1>Calculator:</h1><br><br>`);
+    result = first_number / second_number;
+    document.write(`<h3>Division:</h3>`);
+    document.write(
+      `${first_number} / ${second_number} = ` + result + `<br><br>`
+    );
+  } else if (operator === `%`) {
+    // Modulus / Remainder
+    document.write(`<h1>Calculator:</h1><br><br>`);
+    result = first_number % second_number;
+    document.write(`<h3>Modulus / Remainder</h3>`);
+    document.write(
+      `${first_number} % ${second_number} = ` + result + `<br><br>`
+    );
+  } else if (operator === `+`) {
+    // Addition
+    document.write(`<h1>Calculator:</h1><br><br>`);
+    result = first_number + second_number;
+    document.write(`<h3>Addition:</h3>`);
+    document.write(
+      `${first_number} + ${second_number} = ` + result + `<br><br>`
+    );
+  } else if (operator === `-`) {
+    // Subtration
+    document.write(`<h1>Calculator:</h1><br><br>`);
+    result = first_number - second_number;
+    document.write(`<h3>Subtration:</h3>`);
+    document.write(
+      `${first_number} - ${second_number} = ` + result + `<br><br>`
+    );
+  } else {
+    alert(`Sorry! you missing some Input.`);
+  }
 }
